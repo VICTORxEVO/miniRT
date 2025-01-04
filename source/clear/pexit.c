e1ccd0b4ceca145cae8eaa828fa13dbf1cf2c24c
VICTORxEVO <yassirsbai04@gmail.com>
2025-01-04 11:24:17 +0100

    fix: improve error message formatting in pexit function

--- source/clear/pexit.c
+++ source/clear/pexit.c
@@ -6,7 +6,7 @@
 /*   By: ysbai-jo <ysbai-jo@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 11:44:29 by ysbai-jo          #+#    #+#             */
-/*   Updated: 2025/01/03 13:05:57 by ysbai-jo         ###   ########.fr       */
+/*   Updated: 2025/01/04 11:17:00 by ysbai-jo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 void	pexit(char *msg, short int exit_code)
 {
 	ft_putstr_fd(PRGM_NAME, 2);
-	if (msg[0] == ':')
+	if (msg[ft_strlen(msg) - 1] == '!' || msg[ft_strlen(msg) - 1] == '>')
+	{
 		(ft_putstr_fd(msg, 2), ft_putstr_fd("\n", 2));
+	}
 	else
-		(ft_putstr_fd(": ", 2), perror(msg));
+		(ft_putstr_fd(":", 2), perror(msg));
+	ft_putstr_fd(END, 2);
 	exit(exit_code);
 }
