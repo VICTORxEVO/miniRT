38091caa07cc190445c9d54a8ebcce31e5b5deb7
VICTORxEVO <yassirsbai04@gmail.com>
2025-04-13 16:13:38 +0100

    fix: ensure proper resource cleanup on window close

--- source/rendering/mlx.c
+++ source/rendering/mlx.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mlx.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sgouzi <sgouzi@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ysbai-jo <ysbai-jo@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 18:10:11 by sgouzi            #+#    #+#             */
-/*   Updated: 2025/04/09 18:10:12 by sgouzi           ###   ########.fr       */
+/*   Updated: 2025/04/13 16:07:47 by ysbai-jo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 int	close_window(t_core *engine)
 {
 	mlx_destroy_window(engine->m.mlx, engine->m.win);
+	mlx_destroy_image(engine->m.mlx, engine->img.img);
+	mlx_destroy_display(engine->m.mlx);
 	clear();
 	exit(0);
 	return (0);
@@ -251,6 +253,10 @@ int	key_press(int key, t_core *engine)
 	}
 	else if (key == XK_Escape)
 	{
+		mlx_destroy_window(engine->m.mlx, engine->m.win);
+		mlx_destroy_image(engine->m.mlx, engine->img.img);
+    	mlx_destroy_display(engine->m.mlx);
+		free(engine->m.mlx);
 		clear();
 		exit(0);
 	}
